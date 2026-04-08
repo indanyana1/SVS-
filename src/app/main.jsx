8b7304d2d65ddc3c91685d7e8bfd73029a4bbe56
indanyana <mlulekimzelemu306@gmail.com>
2026-04-08 11:13:18 +0200

    feat: add ScrollToTop component for smooth scrolling on route change

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, useLocation } from 'react-router-dom';
 import '../i18n';
 import App from './App';
+
+function ScrollToTop() {
+	const { pathname } = useLocation();
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathname]);
+	return null;
+}
 
 const rootElement = document.getElementById('root');
 
@@ -10,6 +18,7 @@ if (rootElement) {
 	ReactDOM.createRoot(rootElement).render(
 		<React.StrictMode>
 			<BrowserRouter>
+				<ScrollToTop />
 				<App />
 			</BrowserRouter>
 		</React.StrictMode>
